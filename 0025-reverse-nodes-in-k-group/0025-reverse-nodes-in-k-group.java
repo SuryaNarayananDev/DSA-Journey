df6cca6ac324aae03d8926ca58cd1b64e6b0ce1a
Suryanarayanan <144770284+SuryaNarayananDev@users.noreply.github.com>
2026-06-15 12:17:19 +0530

    Time: 0 ms (100%), Space: 46.6 MB (16.24%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -11,46 +11,49 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         if(head == null) return null;
-        if(head.next == null) return head;
-        List<Integer> arr = new ArrayList<>();
-        ListNode curr = head;
-        while(curr!=null){
-            arr.add(curr.val);
-            curr =curr.next;
-        }
-        
-        curr = head;
-        int count = 1;
-        int prev = 0;
-        for(int i=0;i<arr.size();i++){
-            if(count==k){
-                swap(arr,prev,i);
-                prev = i+1;
-                count=1;
-            }
-            else{
-                count++;
-            }
-        }
+        if(head.next == null || k == 1) return head;
+        boolean firstHead = true;
+        ListNode start = head;
+        ListNode end = head;
+        ListNode before = null;
+        int counter = 1;
 
-        count =0;
-        curr = head;
-        while(curr != null){
-            curr.val = arr.get(count++);
-            curr = curr.next;
+        while(end != null){
+            if(counter == k){
+                ListNode next = end.next;
+                end.next = null;
+                ListNode reverseHead = reverse(start);
+                
+                if(firstHead){
+                    head = reverseHead;
+                    firstHead = false;
+                }else{
+                    before.next = reverseHead;
+                }
+                start.next = next;
+                before = start;
+                start = next;
+                end = next;
+                counter = 1;
+                continue;
+            }
+            end = end.next;
+            counter++;  
         }
 
         return head;
     }
 
-
-    public void swap(List<Integer> arr, int a, int b) {
-        while (a < b) {
-            int temp = arr.get(a);
-            arr.set(a, arr.get(b));
-            arr.set(b, temp);
-            a++;
-            b--;
+    public ListNode reverse(ListNode chunk){
+        ListNode curr = chunk;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
+
+        return prev;
     }
 }
